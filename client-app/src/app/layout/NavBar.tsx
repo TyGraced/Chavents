81cfb8dea00e1127e98a56d9d9460bf3d02c59fa
TyGraced <temitayoadesugba@gmail.com>
2025-06-03 05:09:35 +0100

    Update in Section 21

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -13,7 +13,7 @@ export default observer(function NavBar() {
                     Chavents
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/activities' name="Activities"/>
-                <Menu.Item as={NavLink} to='/errors' name="Errors"/>
+                <Menu.Item as={NavLink} to={`/profiles/${user?.username}`} name="Profile"/>
                 <Menu.Item>
                     <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>       
--- client-app/src/features/activities/dashboard/ActivityFilters.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -1,17 +1,35 @@
+import { observer } from "mobx-react-lite";
 import Calendar from "react-calendar";
 import { Header, Menu, MenuItem } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
 
-export default function ActivityFilters() {
+export default observer(function ActivityFilters() {
+    const {activityStore: {predicate, setPredicate}} = useStore();
     return (
         <>
             <Menu vertical size='large' style={{ width: '100%', marginTop: 28 }}>
                 <Header icon='filter' attached color='teal' content='Filter' />
-                <MenuItem content='All Activities' />
-                <MenuItem content="I'm going" />
-                <MenuItem content="I'm hosting" />
+                <MenuItem 
+                    content='All Activities'
+                    active={predicate.has('all')}
+                    onClick={() => setPredicate('all', 'true')}
+                />
+                <MenuItem 
+                    content="I'm going"
+                    active={predicate.has('isGoing')}
+                    onClick={() => setPredicate('isGoing', 'true')}
+                />
+                <MenuItem 
+                    content="I'm hosting"
+                    active={predicate.has('isHost')}
+                    onClick={() => setPredicate('isHost', 'true')}
+                />
             </Menu>
             <Header />
-            <Calendar />
+            <Calendar 
+                onChange={(date) => setPredicate('startDate', date as Date)}
+                value={predicate.get('startDate') || new Date()}
+            />
         </>
     )
-}
+})
